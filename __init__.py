489ea228b1b1a9b02f9569c2d214bd4fbd42c4a3
Wyatt Roersma <wyatt@simspace.com>
2021-07-27 21:11:07 -0400

    Update __init__.py
    update to apiv2 usage

--- __init__.py
+++ __init__.py
@@ -7,5 +7,5 @@ from aucr_app.plugins.unum.api.file import api_page as unum_api_page
 def load(app):
     """Load overrides for Unum plugin to work properly."""
     app.register_blueprint(unum, url_prefix='/unum')
-    app.register_blueprint(unum_api_page, url_prefix='/api')
+    app.register_blueprint(unum_api_page, url_prefix='/apiv2')
 
